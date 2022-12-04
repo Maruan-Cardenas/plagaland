6264d52ebb94693f1a6968321721e742940793d3
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2022-12-05 00:13:38 +0100

    minor changes in card

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -5,6 +5,9 @@ import styles from './Card.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
+// Motion
+import { motion } from 'framer-motion'
+
 const Card = ({ alt, image, title, description, link }) => {
   return (
     <Link href={`/${link}`}>
@@ -14,6 +17,7 @@ const Card = ({ alt, image, title, description, link }) => {
         </div>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description}>{description}</p>
+        <motion.span className={styles.more}>Ver más</motion.span>
       </article>
     </Link>
   )
